34633edd5e2327a43d9be4f2ca4988f0ac780679
ninikyu <sca.perezm@duocuc.cl>
2026-05-17 17:14:08 -0400

    se agrega perfil test y H2 para CI- probando

--- back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/test/java/com/citt/SpringbootApiRestDespachoApplicationTests.java
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/test/java/com/citt/SpringbootApiRestDespachoApplicationTests.java
@@ -1,13 +1,12 @@
 package com.citt;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringbootApiRestDespachoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
